4b5d47968491765edad4686619050e0ce42700c2
MONTEL SACHA p1908066 <sacha.montel@etu.univ-lyon1.fr>
2021-12-09 10:28:26 +0100

    improvement of the checkout page

--- views/v_shipping.php
+++ views/v_shipping.php
@@ -1,24 +1,40 @@
 <?php require_once(PATH_VIEWS . 'header.php'); ?>
 <?php require_once(PATH_VIEWS . 'menu.php'); ?>
 
-<form action="index.php?page=payment" method="POST">
-    <label for="email"></label>
-    <input name="email" id="email" type="text" placeholder="Email">
+<div class="checkout">
+    <div>
+        <p>&#128293Strong demand! Complete your order before it's too late!</p>
+        <div class="timer">
+            <p>Your cart is reserved for 10:00 minutes.</p>
+        </div>
 
-    <label for="firstname"></label>
-    <input name="firstname" id="firstname" type="text" placeholder="First Name">
-    <label for="lastname"></label>
-    <input name="lastname" id="lastname" type="text" placeholder="Last Name">
+        <div class="address">
+            <h1>1.Shipping address</h1>
+            <form action="index.php?page=payment" method="POST">
+                <input name="email" id="email" type="text" placeholder="Email">
+                <input name="firstname" id="firstname" type="text" placeholder="First Name">
+                <input name="lastname" id="lastname" type="text" placeholder="Last Name">
+                <input name="address" id="address" type="text" placeholder="Address">
+                <input name="zipcode" id="zipcode" type="text" placeholder="Zip Code">
+                <input name="city" id="city" type="text" placeholder="City">
+                <input name="country" id="country" type="text" placeholder="Country">
+            </form>
+        </div>
 
-    <label for="address"></label>
-    <input name="address" id="address" type="text" placeholder="Address">
+        <div class="delivery">
+            <h1>2. Delivery methods</h1>
+            <div>
+                <input type="radio" id="delivery" name="delivery" checked>
+                <label for="devlivery">Free shipping</label>
+            </div>
 
-    <label for="zipcode"></label>
-    <input name="zipcode" id="zipcode" type="text" placeholder="Zip Code">
-    <label for="city"></label>
-    <input name="city" id="city" type="text" placeholder="City">
-    <label for="country"></label>
-    <input name="country" id="country" type="text" placeholder="Country">
+        </div>
+    </div>
 
-    <input type="submit">
-</form>
+
+    <div>
+        <h2>Order Summary</h2>
+        <h1>Payment methods</h1>
+
+    </div>
+</div>
