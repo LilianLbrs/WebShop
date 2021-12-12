318c0440eed97118e103383e6d0f22cea8cbf343
MONTEL SACHA p1908066 <sacha.montel@etu.univ-lyon1.fr>
2021-12-12 11:17:42 +0100

    checkout improvement

--- views/v_shipping.php
+++ views/v_shipping.php
@@ -4,9 +4,28 @@
     <div class="row checkout">
         <div class="col-7">
             <p class="fw-bold mb-2">&#128293Strong demand! Complete your order before it's too late!</p>
-            <div class="timer mb-3">
-                <p class="fw-bold ">Your cart is reserved for 10:00 minutes.</p>
+            <div class="timer mb-3 d-flex">
+                <p class="fw-bold ">Your cart is reserved for
+                <p id="timer" class="ms-1 me-1">10:00</p> minutes.</p>
             </div>
+
+            <script>
+                let minutes = 10
+                let secondes = 0
+                const timerElement = document.getElementById("timer")
+
+                function diminuerTemps() {
+                    if (secondes != 0) secondes--
+                    else {
+                        secondes = 60
+                        minutes--
+                    }
+                    timerElement.innerText = minutes + ":" + secondes
+                }
+
+                if (minutes != 0)
+                    setInterval(diminuerTemps, 1000)
+            </script>
 
             <div class="address mb-3">
                 <p class="fs-5 fw-bolder mb-2">1.Shipping address</p>
@@ -58,7 +77,7 @@
                     </div>
 
                     <div class="d-flex shadow rounded mb-1 bg-white p-2">
-                        <input class="form-check-input me-2" type="radio" name="delivery" id="express">
+                        <input class="form-check-input me-2" type="radio" name="delivery" id="express" onclick="">
                         <label for="express">
                             <p>Express shipping</p>
                             <p>5,00€</p>
@@ -71,20 +90,22 @@
         </div>
 
 
+
+
         <div class="col-5">
-            <p class="fs-5 fw-bolder">Order Summary</p>
+            <p class="fs-5 fw-bolder mb-2">Order Summary</p>
 
             <div class=" container bg-white rounded-top shadow d-flex flex-column pt-3 pb-3 mb-1">
                 <div>
                     <?php
                     foreach ($resultatsItems as $item) {
                     ?>
-                        <div class="d-flex justify-content-between align-items-center">
+                        <div class="d-flex justify-content-between align-items-center mb-2">
                             <div class="d-flex align-items-center">
                                 <div class="position-relative">
                                     <img class="imageItemCheckout rounded me-2" src="<?= PATH_IMAGES . $item['image'] ?>" alt="">
                                     <span class="position-absolute translate-middle badge rounded-pill bg-dark">
-                                        <?= $item['quantity']?>
+                                        <?= $item['quantity'] ?>
                                     </span>
                                 </div>
 
@@ -101,21 +122,29 @@
 
 
                 </div>
-                <div>
-                    <div class="d-flex justify-content-between">
-                        <p>Subtotal</p>
-                        <p><?= $total . "€" ?></p>
-                    </div>
-                    <div class="d-flex justify-content-between">
-                        <p>Delivery</p>
-                        <p>0€</p>
-                    </div>
-                    <div class="d-flex justify-content-between">
-                        <p class="fw-bolder">Total</p>
-                        <p>50€</p>
+
+
+            </div>
+            <div class="container bg-white shadow d-flex flex-column pt-3 pb-3 mb-1">
+                <div class="d-flex justify-content-between mb-1 mt-1">
+                    <p>Subtotal</p>
+                    <div class="d-flex">
+                        <p id="subtotal"><?= $total ?></p>
+                        <p>€</p>
                     </div>
 
                 </div>
+                <div class="d-flex justify-content-between mb-1">
+                    <p>Delivery</p>
+                    <p id="delivery">0€</p>
+                </div>
+                <div class="d-flex justify-content-between mb-1">
+                    <p class="fw-bolder">Total</p>
+                    <p class="fw-bolder" id="total"><?= $total ?></p>
+                </div>
+            </div>
+
+            <div class="container bg-white shadow d-flex flex-column pt-3 pb-3 mb-1">
                 <div class="row">
                     <div class="col-8 ps-0">
                         <input class="form-control me-2" type="text" id="discount" placeholder="Discount code">
@@ -153,7 +182,7 @@
                     <img class="imgPayment" src="<?= PATH_IMAGES . "paypal.png" ?>" alt="">
 
                 </div>
-                <button type="button" class="btn btn-primary" disabled>Complete Purchase</button>
+                <button type="button" class="btn btn-primary" id="purchase">Complete Purchase</button>
 
             </div>
 
@@ -161,3 +190,33 @@
         </div>
     </div>
 </div>
+
+<script>
+    var radio1 = document.getElementById('free');
+    var radio2 = document.getElementById('express');
+    var subtotal = document.getElementById("subtotal").innerText;
+    var total = document.getElementById("total");
+    var delivery = document.getElementById("delivery");
+
+    radio1.addEventListener("click", function() {
+        total.innerText = subtotal + "€";
+        delivery.innerText = "0€";
+    });
+
+    radio2.addEventListener("click", function() {
+        total.innerText = (parseFloat(subtotal) + 5) + "€";
+        delivery.innerText = "5€";
+    });
+
+    var purchase = document.getElementById('purchase');
+    purchase.addEventListener("click", function() {
+        var url = "index.php";
+        var req = new XMLHttpRequest();
+        req.open("POST", url);
+
+
+        req.send(JSON.stringify({
+            adresse: "value"
+        }));
+    });
+</script>
